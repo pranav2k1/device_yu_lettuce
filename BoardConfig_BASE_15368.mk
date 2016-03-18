1a4808822bc62c21146872beff4ceaac7f132212
Ethan Chen <intervigil@gmail.com>
2016-03-17 18:43:41 -0700

    lettuce: Use crackling camera HAL
    * crackling revision: 4b58ba0fb6725b5bc25b1d660ef5286d03965bda
    
    Change-Id: I707668487e64a94c324e09c6d99936f691e574e1

---
+++ BoardConfig_BASE_15368.mk
@@ -0,0 +1,71 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+-include device/cyanogen/msm8916-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/yu/lettuce
+
+TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
+
+# Kernel
+ifneq ($(FORCE_32_BIT),true)
+TARGET_KERNEL_CONFIG := cyanogenmod_lettuce-64_defconfig
+else
+TARGET_KERNEL_CONFIG := cyanogenmod_lettuce_defconfig
+endif
+
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Camera
+BOARD_CAMERA_SENSORS := \
+    ov5670_q5v41b \
+    ov8865_q8v18a
+TARGET_USE_VENDOR_CAMERA_EXT := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# CPU
+TARGET_CPU_CORTEX_A53 := true
+
+# GPS
+TARGET_NO_RPC := true
+USE_DEVICE_SPECIFIC_GPS := true
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_CACHEIMAGE_PARTITION_SIZE := 67108864
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13042155008
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/yu/lettuce/sepolicy
+
+# Tap to wake
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0040/gesture"
+
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# inherit from the proprietary version
+-include vendor/yu/lettuce/BoardConfigVendor.mk
