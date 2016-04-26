8093e73728aea5ec2df7a11a93f186d1a9349ffb
Louis Popi <theh2o64@gmail.com>
2016-04-26 14:04:34 +0530

    lettuce: Set sepolicy to permissive and fix ramdisk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,7 @@ AUDIO_FEATURE_ENABLED_FM := true
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 KERNEL_DEFCONFIG  := yu_lettuce_defconfig
 TARGET_USE_CM_RAMDISK := true
 
